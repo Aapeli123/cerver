12a7873f69cd810b95266358552b8785da9990db
Aapo Harju <aaposharju@gmail.com>
2023-02-28 13:13:25 +0200

    Skeleton of the config parser written

--- src/config/config.c
+++ src/config/config.c
@@ -1,7 +1,23 @@
 #include "config.h"
 
 static void parse_config_line(char* line) {
-
+    char* command = strtok(line, " ");
+    if(!strcmp(command, "loc")) {
+        // Configure page(s) for a location
+    } else if(!strcmp(command, "port")) {
+        // Set the server port
+    } else if(!strcmp(command, "header")) {
+        // Set http header
+    } else if(!strcmp(command, "fallback")) {
+        // Fallback page (Error 404)
+    } else if(!strcmp(command, "root")) {
+        // Set the root dir where to look for pages
+    } else if(!strcmp(command, "include")) {
+        // Include another config file
+    } else {
+        printf("Failed to parse line: %s. Unknown command.\n", line);
+        return;
+    }
 }
 
 void config_read(const char* path) {
--- src/config/config.h
+++ src/config/config.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void config_read(const char* path);
 #endif
