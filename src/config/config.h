a41fdbdedc9d5f1273e6e49bd52a18421b41beac
Aapo Harju <aaposharju@gmail.com>
2023-03-02 13:48:24 +0200

    config struct

--- src/config/config.h
+++ src/config/config.h
@@ -4,8 +4,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include "hashmap/hashmap.h"
 
 #include "commands.h"
+
+struct Config
+{
+    int port;
+    char* root_dir;
+    char* fallback_page;
+    hashmap_t* route_map;
+    hashmap_t* header_map;
+    bool ssl;
+};
+
+typedef struct Config config_t;
+
 
 void config_read(const char* path);
 #endif
