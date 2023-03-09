acb2da680e039158faeb4ec759b89c5a2644f9d2
Aapo Harju <aaposharju@gmail.com>
2023-03-09 13:05:17 +0200

    Remembering to free the memory allocated to config headers

--- src/config/config_t.c
+++ src/config/config_t.c
@@ -17,6 +17,12 @@ config_t *config_create() {
 
 void config_destroy(config_t* c) {
     // hashmap_destroy(c->header_map);
+    for (int i = 0; i < c->header_count; ++i) {
+        free(c->headers[i]->key);
+        free(c->headers[i]->value);
+        free(c->headers[i]);
+    }
+    free(c->headers);
     hashmap_destroy(c->route_map);
     free(c->fallback_page);
     free(c->root_dir);
